3bc1ca00aef342ef305a61f136402b64542efd63
devo4ka <2am9wp@gmail.com>
2023-12-17 13:23:40 +0100

    ej1js

---
+++ S1/1.variables/exercise-3.js
@@ -0,0 +1,4 @@
+var x=5;
+var y=10;
+z=x+y;
+print("El valor de z es:", z)
---
+++ S1/1.variables/exercise-6.js
@@ -0,0 +1,4 @@
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+sumaTotal=toy1.price + toy2.price;
+console.log("la suma total de los precios es",sumaTotal)
